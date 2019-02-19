6cda176a4f4b13261ea4d2074f09f182a63a7023
Jonathan McFadden <jonathan.mcfadden@repknight.com>
2019-02-19 17:09:26 +0000

    css stuff

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <h2 className="header">birb footer</h2>
+   <p>Birb Footer</p>
   </div>
 );
 
--- src/components/tasks/TaskItem.js
+++ src/components/tasks/TaskItem.js
@@ -4,24 +4,39 @@ const styles = {
   hint: {
     color: "goldenrod",
     display: "block",
-    "margin-left": "40px",
+    "margin-left": "20px",
     "margin-top": "15px",
     "font-size": "13.5px",
     opacity: "0.8"
   },
   bullet: {
     "padding-right": "10px"
+  },
+  task: {
+    color: "white"
+  },
+  taskItemContainer: {
+    display: "grid",
+    "grid-template-columns": "40px auto",
+    padding: "5px",
+    background: "#2b333c",
+    "border-radius": "20px",
+    "margin-bottom": "20px"
   }
 };
 
 const TaskItem = ({ task, hint }) => (
-  <p>
-    <span style={styles.bullet} className="tooltip" role="img" aria-label="birb">
-      🐦
-    </span>
-    {task}
-    <span style={styles.hint}>{hint}</span>
-  </p>
+  <div style={styles.taskItemContainer}>
+    <p>
+      <span style={styles.bullet} className="tooltip" role="img" aria-label="birb">
+        🐦
+      </span>
+    </p>
+    <p>
+      <span style={styles.task}>{task}</span>
+      <span style={styles.hint}>{hint}</span>
+    </p>
+  </div>
 );
 
 export default TaskItem;
