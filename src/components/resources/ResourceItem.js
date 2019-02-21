7205fcdf5defd79215ac0744330e525c54961961
Jonathan McFadden <jonathan.mcfadden@repknight.com>
2019-02-21 14:46:00 +0000

    new tab opening

--- src/components/resources/ResourceItem.js
+++ src/components/resources/ResourceItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const ResourceItem = ({ name, url }) => (
   <div className="resourceItemContainer">
     <p className="resourceName">{name} </p>
-    <p className="resourceUrl"><a href={url}>{url}</a></p>
+    <p className="resourceUrl"><a href={url} target='_blank' rel="noopener noreferrer">{url}</a></p>
   </div>
 );
 
