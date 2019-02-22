ad6cc8dd4ec3725077740402c51f55524d436718
Jonathan McFadden <jonathan.mcfadden@repknight.com>
2019-02-22 09:43:26 +0000

    redux provider

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,15 @@
 import React             from "react";
 import { BrowserRouter } from "react-router-dom";
 import App               from "./App";
+import { Provider }      from 'react-redux';
+import store             from "../redux/store";
 
 const Routes = () => (
-  //   <Provider store={store}>
+<Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  //   </Provider>
+</Provider>
 );
 
 export default Routes;
